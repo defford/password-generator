8d83bcb9b02522f27c0143852f76eca46c063187
defford <defford@gmail.com>
2025-01-19 13:47:51 -0330

    Added alternative character type flags
    Added the ability to specify l, u, n, or s to include various character types
    It doesn't work yet, not registering validOptions when parsing chars in flag.

--- password-generator.js
+++ password-generator.js
@@ -2,7 +2,29 @@ const process = require("process");
 const args = process.argv.slice(2);
 
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
-let string = lowercase;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+const validOptions = ["l", "u", "n", "s"];
+
+// Initialize possible password characters to all lowercase and length to 8
+let string = "";
+let length = 8;
+
+const helpMessage = `
+Usage: node password-generator [options]
+
+Options:
+  --[num]      Set the length of password
+  --[a || l &| u &| n &| s]  Set the character type by specifying the first letter of type
+  (a = all, l = lowercase, u = uppercase, n = number,  s = special character)
+
+Example:
+  node password-generator --10 (Generates a 10 character password)
+  node password-generator --6 -lu (Generates a 6 character password with lowercase and uppercase letters)
+  node password-generator --a (Generates an 8 character password with all character types)
+  `;
 
 const randomizer = (string) =>
   string[Math.floor(Math.random() * string.length)];
@@ -15,30 +37,81 @@ const passwordGenerator = (length, string) => {
     password += letter;
   }
 
-  return password;
+  return console.log(password);
 };
 
-const helpMessage = `
-Usage: node password-generator [options]
+if (args.length === 0) {
+  string = lowercase;
+} else if (args.length === 1) {
+  string = lowercase;
 
-Options:
-  --[num]      Set the length of password
-
-Example:
-  node password-generator --10 (Generates a 10 character password)
-   
-  `;
-
-if (args[0] === "--help") {
-  console.log(helpMessage);
-}
-
-if (/^--\d+$/.test(args[0])) {
-  const length = parseInt(args[0].slice(2), 10);
-  console.log(length);
-  if (!isNaN(length)) {
-    console.log(passwordGenerator(length, string));
+  if (!isNaN(parseInt(args[0].slice(2)))) {
+    length = parseInt(args[0].slice(2));
   }
-} else {
-  console.log(helpMessage);
+
+  switch (args[0]) {
+    case "--help":
+      console.error(helpMessage);
+      process.exit(1);
+    case "--a":
+      string = lowercase + uppercase + numbers + specialCharacters;
+      break;
+    case args[0].includes(validOptions):
+        console.log("includes validOptions");
+        for (let char of args[0]) {
+            if (validOptions.includes(char)) {
+                switch (char) {
+                    case "l":
+                        string += lowercase;
+                        break;
+                    case "u":
+                        string += uppercase;
+                        break;
+                    case "n":
+                        string += numbers;
+                        break;
+                    case "s":
+                        string += specialCharacters
+                        break;
+                    default:
+                        break;
+
+            }
+            }
+        }
+  }
 }
+
+//   if (args.includes("--a")) {
+//     string += lowercase + uppercase + numbers + specialCharacters;
+//   } else {
+//     const validOptions = ["l", "u", "n", "s"];
+//     for (i = 0; i < args.length; i++) {
+
+//       const option = args[i];
+//       if (isNaN(parseInt(option.slice(2)))) {
+
+//         for (let char of option) {
+
+//           // Print help message if the flags contain non-valid options
+//           if (!validOptions.includes(char)) {
+//             console.error(helpMessage);
+//             process.exit(1);
+//           }
+
+//           if (option.includes("l")) string += lowercase;
+//           if (option.includes("u")) string += uppercase;
+//           if (option.includes("n")) string += numbers;
+//           if (option.includes("s")) string += specialCharacters;
+
+//         }
+//       } else {
+
+//         length = parseInt(option.slice(2));
+
+//       }
+//     }
+//   }
+// }
+
+passwordGenerator(length, string);
